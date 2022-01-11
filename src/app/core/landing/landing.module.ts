11891828d16e366e5aa86213b28e7ab52888b708
santiago-jv <santiagoolayojv@gmail.com>
2022-01-11 06:49:44 -0500

    footer section finished

--- src/app/core/landing/landing.module.ts
+++ src/app/core/landing/landing.module.ts
@@ -5,9 +5,10 @@ import { MainComponent } from './sections/main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { ForTheFutureComponent } from './sections/for-the-future/for-the-future.component';
 import { StateOfTheArtComponent } from "./sections/state-of-the-art/state-of-the-art.component";
+import { ExtraInfoComponent } from './sections/extra-info/extra-info.component';
 
 @NgModule({
-    declarations:[LandingComponent, HeaderComponent, MainComponent, FooterComponent, ForTheFutureComponent, StateOfTheArtComponent],
+    declarations:[LandingComponent, HeaderComponent, MainComponent, FooterComponent, ForTheFutureComponent, StateOfTheArtComponent, ExtraInfoComponent],
     imports:[],
     exports:[LandingComponent]
 })
