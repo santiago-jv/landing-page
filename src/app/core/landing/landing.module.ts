f086a738aae690e8e9b12245b4b029bbb1e9ffe1
santiago-jv <santiagoolayojv@gmail.com>
2022-01-11 05:50:33 -0500

    state of the art section finished

--- src/app/core/landing/landing.module.ts
+++ src/app/core/landing/landing.module.ts
@@ -4,9 +4,10 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './sections/main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { ForTheFutureComponent } from './sections/for-the-future/for-the-future.component';
+import { StateOfTheArtComponent } from "./sections/state-of-the-art/state-of-the-art.component";
 
 @NgModule({
-    declarations:[LandingComponent, HeaderComponent, MainComponent, FooterComponent, ForTheFutureComponent],
+    declarations:[LandingComponent, HeaderComponent, MainComponent, FooterComponent, ForTheFutureComponent, StateOfTheArtComponent],
     imports:[],
     exports:[LandingComponent]
 })
---
+++ src/app/core/landing/sections/state-of-the-art/state-of-the-art.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-state-of-the-art',
+    templateUrl:'state-of-the-art.component.html',
+    styleUrls:['state-of-the-art.component.css']
+})
+
+export class StateOfTheArtComponent{}
